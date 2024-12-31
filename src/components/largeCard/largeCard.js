72bccc053e6a450c7a1077b62a041309a86d3c5b
Omniladder <dustintobrien@gmail.com>
2024-12-31 11:38:31 -0500

    Increased Glow Size

--- src/components/largeCard/largeCard.js
+++ src/components/largeCard/largeCard.js
@@ -9,7 +9,7 @@ function largeCard({title, quote, image}) {
   return (
     <Card sx={{ width: '30vw' , height: '35vw', backgroundColor: '#A61C3C', borderRadius: '10%', border: '5px solid black', borderColor: '#FF9B71', transition: 'transform 0.25s ease-in-out', boxShadow: '15px 13px 8px rgba(0, 0, 0, 0.5)', 
       '&:hover': { 
-        boxShadow: '0 0 14px 0px #A61C3C', cursor: 'pointer', transformOrigin: 'center center', willChange: 'transform',}
+        zIndex: 10,boxShadow: '0 0 90px 0px #A61C3C', cursor: 'pointer', transformOrigin: 'center center', willChange: 'transform',}
       }}>
 
       <Typography sx={{ fontSize: 30, color: '#D8D78F', fontFamily: 'Rubik, sans-serif', fontWeight: 500}} gutterBottom>
