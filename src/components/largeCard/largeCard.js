f4caaba4f2eceac7fdd009d45e4e4e78e592cae1
Omniladder <dustintobrien@gmail.com>
2025-01-01 15:11:40 -0500

    Working Expandable button on test

--- src/components/largeCard/largeCard.js
+++ src/components/largeCard/largeCard.js
@@ -1,24 +1,51 @@
 import  React from 'react';
-import Card from '@mui/material/Card';
-import Typography from '@mui/material/Typography';
 import { useState } from 'react';
 
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import { motion } from "framer-motion";
 
 import './largeCard.css';
 
 function LargeCard({title, quote, image}) {
-  const [isCardOpened, setCardOpened] = useState(false);
+  const [isCardOpened, setCardOpened] = useState(true);
+  const [cardPosition, setCardPosition] = useState({ width: '30vw', height: '35vw' });
+
+
   return (
-    <Card sx={{ 
-      
+    <Card 
+    
+    //Clickable Functionality
+    isCardOpened = {isCardOpened} 
+    onClick = {() => 
+    {
+      setCardOpened(!isCardOpened);
+
+    }}
+    
+    layout
+
+    sx={{   
       //CSS Styling
-      width: '30vw' , zIndex: 10, height: '35vw', backgroundColor: '#A61C3C', borderRadius: '10%', border: '5px solid black', borderColor: '#FF9B71', transition: 'transform 0.25s ease-in-out', boxShadow: '15px 13px 8px rgba(0, 0, 0, 0.5)', 
-      '&:hover': {  transform: 'scale(1.05)', zIndex: 10,boxShadow: '0 0 90px 0px #A61C3C', cursor: 'pointer', transformOrigin: 'center center', willChange: 'transform',},
+      zIndex: 10, backgroundColor: '#A61C3C', borderRadius: '10%', border: '5px solid black', borderColor: '#FF9B71',  boxShadow: '15px 13px 8px rgba(0, 0, 0, 0.5)', 
+
+      margin: 'auto',
+
+      //Transistion Speeds
+      transition: 'transform 0.25s ease-in-out',
+      transition: "all 0.5s ease",
+
+      //Adjusted Section of CSS
+  
+      position: isCardOpened ? 'relative' : 'fixed', inset: isCardOpened ? 'auto' : '0 0 0 0',
       
-      //Clickable Functionality
-      //isCardOpened : {isCardOpened}, onClick={() => setCardOpened(!isCardOpened)} ,
       
-      }}>
+      width: isCardOpened ? '30vw' : '60vw' , height: isCardOpened ? '35vw' : '50vw', 
+      '&:hover': isCardOpened ? {  transform: 'scale(1.05)', zIndex: 10,boxShadow: '0 0 90px 0px #A61C3C', cursor: 'pointer',  transformOrigin: 'center center', willChange: 'transform',} : {cursor: 'pointer'},
+      
+      }
+      
+      }>
 
       <Typography sx={{ fontSize: 30, color: '#D8D78F', fontFamily: 'Rubik, sans-serif', fontWeight: 500}} gutterBottom>
           {title}
@@ -32,5 +59,9 @@ function LargeCard({title, quote, image}) {
     </Card>
   );
 }
+
+
+
+
 
 export default LargeCard;
